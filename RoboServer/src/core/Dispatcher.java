6e625bfa0dadbf26e825aa7a6a7905a21cf75a5a
Raphael Vogel <raphael.vogel@>
2013-01-23 20:56:39 +0100

    Change drive ui to use icons
    Change-Id: I1ac751731f0a08582f4c25fea5686eb35dee20dd

--- RoboServer/src/core/Dispatcher.java
+++ RoboServer/src/core/Dispatcher.java
@@ -27,7 +27,7 @@ public class Dispatcher extends HttpServlet {
 			}	
 			handler.serve(action, request, response);
 		} catch (Exception e) {
-			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
 			response.getWriter().println("Could not initialize system");
 			return;
 		}
