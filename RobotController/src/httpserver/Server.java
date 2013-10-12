12ec241614b2e5b64b58eac6d9aa197ff3c8289e
Raphael Vogel <raphael.vogel@>
2013-10-12 11:05:02 +0200

    RobotController initial

---
+++ RobotController/src/httpserver/Server.java
@@ -0,0 +1,29 @@
+package httpserver;
+
+import java.util.Map;
+
+public class Server extends NanoHTTPD{
+
+    public Server(String host, int port) {
+        super(host, port);
+    }
+
+    @Override
+    public Response serve(String uri, Method method, Map<String, String> header, Map<String, String> parms, Map<String, String> files) {
+        System.out.println(method + " '" + uri + "' ");
+
+        String msg = "<html><body><h1>Hello server</h1>\n";
+        if (parms.get("username") == null)
+            msg +=
+                    "<form action='?' method='get'>\n" +
+                            "  <p>Your name: <input type='text' name='username'></p>\n" +
+                            "</form>\n";
+        else
+            msg += "<p>Hello, " + parms.get("username") + "!</p>";
+
+        msg += "</body></html>\n";
+
+        return new NanoHTTPD.Response(msg);
+    }
+
+}
